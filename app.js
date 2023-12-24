3f2070d814da47def3a8333f758603897a1f9f18
Meghana Dhanani <136313785+Meghanadhanani-1904@users.noreply.github.com>
2023-12-24 19:47:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,27 +6,26 @@ let list = document.querySelector(".list");
 addBtn.addEventListener("click", () => {
   if (input.value == "") {
     alert("Add something");
-  }
- else if (input.value.length<4) {
+  } else if (input.value.length < 4) {
     alert("Your Task should have at least 4 letters");
-  }else {
+  } else {
     let newDiv = document.createElement("div");
     newDiv.className = "list";
     listContainer.appendChild(newDiv);
 
-    let items=document.createElement("div");
-    items.className="item";
+    let items = document.createElement("div");
+    items.className = "item";
     newDiv.appendChild(items);
 
     let newH1 = document.createElement("h1");
     newH1.innerText = input.value;
+    newH1.className = "h1Text";
     items.appendChild(newH1);
 
     let delBtn = document.createElement("button");
     delBtn.className = "delete";
     delBtn.innerHTML = "Delete";
     newDiv.append(delBtn);
-
   }
   input.value = "";
   saveData();
@@ -38,7 +37,7 @@ listContainer.addEventListener(
     // console.log("click", e.target);
     // console.log("click", e.target.parentElement);
 
-    if (e.target.className === "a") {
+    if (e.target.className === "h1Text") {
       e.target.style.textDecoration = "line-through";
       saveData();
     } else if (e.target.className === "delete") {
